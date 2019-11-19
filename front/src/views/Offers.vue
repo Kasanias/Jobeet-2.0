42889214ceaa4e82538fe23d71cf0ad939a07d92
Esteban Casanas <esteban.ca19@gmail.com>
2019-11-19 16:47:09 +0100

    Offers: added firebase request, working

--- front/src/views/Offers.vue
+++ front/src/views/Offers.vue
@@ -1,5 +1,35 @@
 <template>
   <div>
     <h1>Offers</h1>
+    <ul>
+      <li v-for="item in this.offers">{{ item.name }}</li>
+    </ul>
   </div>
 </template>
+
+<script>
+import { auth, db } from "@/main";
+import router from "../router/index";
+import store from "../store/index";
+
+export default {
+  data() {
+    return {
+      offers: []
+    };
+  },
+  methods: {},
+  created() {
+    db.collection("offers")
+      .get()
+      .then(snapshot => {
+        if (snapshot.empty) {
+          return;
+        }
+        snapshot.forEach(doc => {
+          this.offers.push(doc.data());
+        });
+      });
+  }
+};
+</script>
