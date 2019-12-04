21079fadefed63f399a1398de7b968ecd5b8e807
Esteban Casanas <esteban.ca19@gmail.com>
2019-12-04 12:13:06 +0100

    Dashboard: added my applications as an applicant

--- front/src/views/Dashboard.vue
+++ front/src/views/Dashboard.vue
@@ -1,17 +1,16 @@
 <template>
   <div>
     <div class="row">
-      <div class="col-sm-8 ml-3">
-        <h3>Mes offres</h3>
-        <div class="row">
-          <div class="col">
-            <h3>1</h3>
+      <div v-if="user.profile === 'applicant'" class="col-sm">
+        <div class="card text-center m-3">
+          <h3 class="card-header">My applications</h3>
+          <div class="card-body">
+            <div class="row ml-2 mr-2">
+              <mini-offer v-for="item in this.myOffersPage" :offer="item"></mini-offer>
+            </div>
           </div>
-          <div class="col">
-            <h3>1</h3>
-          </div>
-          <div class="col">
-            <h3>1</h3>
+          <div class="card-footer pb-0 pt-3">
+            <jw-pagination :items="myOffers" @changePage="onChangePage" :pageSize="pageSize"></jw-pagination>
           </div>
         </div>
       </div>
@@ -26,11 +25,48 @@
 
 <script>
 //mes offres, suggestions à droite profil
-import { db } from "@/main";
+import { auth, db } from "@/main";
+import router from "../router/index";
+import store from "../store/index";
+import MiniOffer from "../components/MiniOffer";
 export default {
-  data() {
-    return {};
+  components: {
+    MiniOffer
   },
-  methods: {}
+  data() {
+    return {
+      user: {},
+      myOffers: [],
+      myOffersPage: [],
+      pageSize: 4
+    };
+  },
+  created() {
+    db.collection("users")
+      .doc(store.getters.getUser)
+      .get()
+      .then(doc => {
+        this.user = doc.data();
+      })
+      .catch(err => {
+        console.log("Error getting document", err);
+      });
+  },
+  mounted() {
+    db.collection("offers")
+      .where("applicants", "array-contains", store.getters.getUser)
+      .get()
+      .then(querySnapshot => {
+        this.myOffers = querySnapshot.docs.map(doc => doc.data());
+      })
+      .catch(function(error) {
+        console.log("Error getting offers: ", error);
+      });
+  },
+  methods: {
+    onChangePage(itemsPage) {
+      this.myOffersPage = itemsPage;
+    }
+  }
 };
 </script>
