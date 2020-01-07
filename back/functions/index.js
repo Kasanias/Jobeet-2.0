e7768f0d976805ed6ced90071c24c2be0a4206c7
Maxime L'Hermite <maxime.lhermite@epita.fr>
2020-01-07 17:11:14 +0100

    edit back (forgot to commit, oups)

--- back/functions/index.js
+++ back/functions/index.js
@@ -1,13 +1,27 @@
+const admin = require('firebase-admin');
 const functions = require('firebase-functions');
-const express = require('express');
-const cors = require('cors')({origin: true});
-const app = express();
-
-app.use(cors);
 
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
+const config = {
+    apiKey: "AIzaSyDdBrcA_slQq6owvm3Q5_waRJAA0PjyzhI",
+    authDomain: "nlpf-1570028868321.firebaseapp.com",
+    databaseURL: "https://nlpf-1570028868321.firebaseio.com",
+    projectId: "nlpf-1570028868321",
+    storageBucket: "nlpf-1570028868321.appspot.com",
+    messagingSenderId: "811645163630",
+    appId: "1:811645163630:web:5af5ebe66a950db88a6e7f",
+    measurementId: "G-CDS8G5F4XD"
+};
+admin.initializeApp(config);
+let db = admin.firestore();
+
+exports.ping = functions.https.onRequest(async (req, res) => {
+    res.json("pong");
 });
 
-exports.app = functions.https.onRequest(app);
+
+exports.iot = functions.https.onRequest(async (req, res) => {
+    let data = req.body;
+    db.collection('iot-data').add(data);
+    res.send("Added to database: " + JSON.stringify(req.body));
+});
